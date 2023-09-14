414f309284d842b68a52e219779123b5385f6e5a
Roolps <yara@clubnode.com>
2023-09-14 22:05:10 +0000

    Simple bar graph generation

--- graph.go
+++ graph.go
@@ -3,12 +3,10 @@ package gograph
 import (
 	"errors"
 	"fmt"
-	"log"
 	"math"
 	"os"
 	"sort"
 	"text/template"
-	"time"
 )
 
 // Available graph types
@@ -35,7 +33,7 @@ type Graph struct {
 	// Dataset for the graph
 	DataSet *[]map[string]float64
 
-	Settings *graphSettings `json:"settings"`
+	Settings *GraphSettings `json:"settings"`
 }
 
 type GraphAxis struct {
@@ -58,8 +56,12 @@ type GraphAxis struct {
 	Labels []float64
 }
 
-type graphSettings struct {
+type GraphSettings struct {
 	Labels bool `json:"labels,omitempty"`
+
+	Fill       string `json:"fill,omitempty"`
+	Background string `json:"background,omitempty"`
+	Radius     int64  `json:"radius,omitempty"`
 }
 
 // Create new go graph!
@@ -67,15 +69,55 @@ func (g *Graph) New(exportPath string) (string, error) {
 	switch g.GraphType {
 	case 1:
 		// ok... g, err :=
-		g, err := g.newAxis(g.YAxis.Key, float64(g.Height))
+		g, err := g.newAxis(g.YAxis.Key, g.XAxis.Key, float64(g.Height))
 		if err == nil {
-			t := template.Must(template.New("").Parse(`
-			<svg viewBox="0 0 {{.Width}} {{.Height}}" width="{{.Width}}" height="{{.Height}}" fill="red" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">       
-			
-			{{range $l := .YAxis.Labels}}<text>{{$l}}</text>{{end}}
-			
-			</svg>`))
-			f, err := os.Create(exportPath + "/" + fmt.Sprintf("%x", time.Now().Unix()) + ".svg")
+			// w/h * h = w
+			t := template.Must(template.New("").Funcs(template.FuncMap{
+				"calcY": func(max float64, val float64) float64 {
+					return max - val
+				},
+				"calcX": func(i int, increment float64) float64 {
+					return float64(i) * increment
+				},
+				"labelY": func(height int64) float64 {
+					return 40 - (float64(height) / 2)
+				},
+			},
+			).Parse(`
+			<div id="graph-container" style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%)">
+				<div style="display:flex;flex-direction:row;">
+					<div class="y-labels" style="position:relative;display:flex;height:{{.Height}}px;justify-content:space-between;flex-direction:column-reverse;width:30px;align-items:flex-end;margin-right:5px;">
+						{{if not (eq .Settings nil)}}
+						{{if .Settings.Labels}}
+						<p class="axis-label" style="position:absolute;width:{{.Height}}px;top:50%;right:{{labelY .Height}}px;transform:translateY(-50%) rotate(-90deg);text-align:center;margin:0;">{{.YAxis.Label}}</p>
+						{{end}}
+						{{end}}
+						{{range $l := .YAxis.Labels}}
+						<p style="margin: 0;">{{$l}}</p>
+						{{end}}
+					</div>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {{.XAxis.Max}} {{.YAxis.Max}}" width="{{.Width}}" height="{{.Height}}" preserveAspectRatio="none" {{if not (eq .Settings nil)}}fill="{{.Settings.Fill}}" style="background-color:{{.Settings.Background}}"{{end}}>       
+						{{$xAxis := .XAxis}}
+						{{$yAxis := .YAxis}}
+						{{$settings := .Settings}}
+						{{range $i, $d := .DataSet}}
+						<rect width="{{$xAxis.Increment}}" height="{{index $d $yAxis.Key}}" x="{{calcX $i $xAxis.Increment}}" y="{{calcY $yAxis.Max (index $d $yAxis.Key)}}" {{if not (eq $settings nil)}}rx="{{$settings.Radius}}"{{end}}/>
+						{{end}}
+					</svg>				
+				</div>
+				<div class="x-labels" style="position:relative;display:flex;flex-direction:row;width:{{.Width}}px;justify-content:space-between;margin-left:30px;margin-top:5px;">
+					{{if not (eq .Settings nil)}}
+					{{if .Settings.Labels}}
+					<p class="axis-label" style="position:absolute;width:{{.Width}}px;left:50%;transform:translateX(-50%);text-align:center;bottom:-25px;margin:0;">{{.XAxis.Label}}</p>
+					{{end}}
+					{{end}}
+					{{range $i, $d := .DataSet}}
+					<p style="width:calc(100% / {{len $xAxis.Labels}});text-align:center;margin:0;">{{index $d $xAxis.Key}}</p>
+					{{end}}
+				</div>
+			</div>
+			`))
+			f, err := os.Create(exportPath + "/testing.html")
 			if err != nil {
 				return "", errors.New("create file: " + err.Error())
 			}
@@ -94,8 +136,9 @@ func (g *Graph) New(exportPath string) (string, error) {
 }
 
 // Generate friendly axis from dataset
-func (g *Graph) newAxis(key string, dimension float64) (*Graph, error) {
-	keys := []float64{}
+func (g *Graph) newAxis(Ykey string, XKey string, dimension float64) (*Graph, error) {
+	yKeys := []float64{}
+	xKeys := []float64{}
 
 	if g == nil {
 		return g, errors.New("declared graph is nill")
@@ -106,21 +149,33 @@ func (g *Graph) newAxis(key string, dimension float64) (*Graph, error) {
 
 	// Grab all values in dataset and order them
 	for _, d := range *g.DataSet {
-		keys = append(keys, d[key])
+		yKeys = append(yKeys, d[Ykey])
+
+		xKeys = append(xKeys, d[XKey])
+		g.XAxis.Labels = append(g.XAxis.Labels, d[XKey])
 	}
-	sort.Float64s(keys)
+	sort.Float64s(yKeys)
+	sort.Float64s(xKeys)
 
 	// Get the increment factor (1:10 = 1, 10:100 = 10, 100:1000 = 100) etc
-	log.Println(keys[len(keys)-1])
-	max := math.Ceil(math.Log10(keys[len(keys)-1]*1.2)) - 1
+	max := math.Ceil(math.Log10(yKeys[len(yKeys)-1]*1.15)) - 1
 
-	g.YAxis.Max = roundToMult(keys[len(keys)-1]*1.2, math.Pow(10, max))
+	// Set max value around 20% above the top value rounded to the closest multiple of the increment
+	g.YAxis.Max = roundToMult(yKeys[len(yKeys)-1]*1.15, math.Pow(10, max))
 	g.YAxis.Increment = g.YAxis.Max / (dimension / 40)
 
+	// Generate the labels for the Y axis following the increment
 	g.YAxis.Labels = *increment(g.YAxis.Increment, int(dimension/40))
-	log.Println(g.YAxis.Labels)
+
+	g.XAxis.Max = float64(g.Width) / float64(g.Height) * g.YAxis.Max
+	g.XAxis.Increment = g.XAxis.Max / float64(len(xKeys))
+
 	return g, nil
 }
+
+// ------------------
+// UTILITY FUNCTIONS
+// ------------------
 
 func roundToMult(number float64, multiple float64) float64 {
 	return math.Ceil(number/multiple) * multiple
@@ -132,4 +187,15 @@ func increment(increment float64, max int) *[]float64 {
 		labels = append(labels, float64(i)*increment)
 	}
 	return &labels
+}
+
+func reverse[T any](s []T) []T {
+	f := 0
+	l := len(s) - 1
+	for f < l {
+		s[f], s[l] = s[l], s[f]
+		f++
+		l--
+	}
+	return s
 }
